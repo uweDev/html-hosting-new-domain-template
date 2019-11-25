6ecbefba8e02f93c3309b234ccbb4d3ac15bf811
uweDev <58147568+uweDev@users.noreply.github.com>
2019-11-25 23:43:26 +0200

    improve readability

--- index.php
+++ index.php
@@ -9,50 +9,50 @@ This open source software was released on 24.11.2019 by Uwe Hellmann and is subj
 You can download this Open-Source-Software from here: https://github.com/uweDev/html-hosting-new-domain-template
 -->
     <head>
-        <meta charset="utf-8">
+	<meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Domain reserved</title>
-		<style>
-			* {
-				margin: 0;
-				padding: 0;
-			}
-			body {
-				font-family: Open Sans, sans-serif;
-			}
-			h2 {
-				font-size: 15px;
-				font-weight: 900;
-				color: #a00c57;
-			}
-			p {
-				font-size: 20px;
-				margin-bottom: 40px;
-			}
-			p + h2 + p {
-				margin-bottom: 0;
-			}
-			.text-center {
-				text-align: center;
-			}
-			header {
-				background-color: #e6e6e6;
-				padding: 30px 10px;
-			}
-			.reservedDomainContent {
-				padding: 12% 30%;
-			}
-			footer {
-				background-color: #a00c57;
-				color: #fff;
-				padding: 60px 10px;
-			}
-			footer a {
-				text-decoration: none;
-				color: #fff;
-			}
-		</style>
+	<style>
+		* {
+			margin: 0;
+			padding: 0;
+		}
+		body {
+			font-family: Open Sans, sans-serif;
+		}
+		h2 {
+			font-size: 15px;
+			font-weight: 900;
+			color: #a00c57;
+		}
+		p {
+			font-size: 20px;
+			margin-bottom: 40px;
+		}
+		p + h2 + p {
+			margin-bottom: 0;
+		}
+		.text-center {
+			text-align: center;
+		}
+		header {
+			background-color: #e6e6e6;
+			padding: 30px 10px;
+		}
+		.reservedDomainContent {
+			padding: 12% 30%;
+		}
+		footer {
+			background-color: #a00c57;
+			color: #fff;
+			padding: 60px 10px;
+		}
+		footer a {
+			text-decoration: none;
+			color: #fff;
+		}
+	    </style>
 	</head>
 	<body>
 		<div class="reservedDomainContainer">
